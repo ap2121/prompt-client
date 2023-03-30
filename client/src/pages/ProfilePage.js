9df159298164ffe5c967e627537dcca96789e9fa
Arthur Pate <arthurpate42@gmail.com>
2023-03-30 09:51:37 -0700

    user profile able to render all information needed for profile page, mapping needed for followers following and posts

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -19,7 +19,31 @@ const ProfilePage = () => {
   }, [id])
   
   return (
-    <div>Hello</div>
+    <div>
+      <h1 style={{color: 'white'}}>{currentUser?.bio}</h1>
+      <img src={currentUser?.proPic}/>
+      {currentUser?.Posts?.map((p) => (
+        <div key={p.id}>
+          
+          <img src={p.imgRes}/>
+          <p style={{color: 'white'}}> {p.capRes} </p>
+        </div>
+      ))}
+       <p style={{color: 'white'}}>Followers:</p><br></br>
+       {currentUser?.Followers?.map((f) => (
+        <div key={f.id}>
+        <p style={{color: 'white'}}>{f.username}</p>
+        </div>
+      ))}
+      <br></br>
+      <p style={{color: 'white'}}>Following:</p><br></br>
+      {currentUser?.Following?.map((f) => (
+        <div key={f.id}> 
+        <p style={{color: 'white'}}>{f.username}</p>
+        </div>
+      ))}
+      </div>
+
   )
 }
 
