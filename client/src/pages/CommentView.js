0d75367eb1886436676d4ae9f542341783f78c0b
Arthur Pate <arthurpate42@gmail.com>
2023-03-30 18:16:17 -0700

    user now able to view all comments on a post, unable to show username next to the comment, will need to refactor possibly

--- client/src/pages/CommentView.js
+++ client/src/pages/CommentView.js
@@ -3,8 +3,8 @@ import {useState, useEffect} from 'react'
 import {useParams, useNavigate} from 'react-router-dom'
 import Client from '../services/Api'
 const CommentView = () => {
-  const [post, setPost] = useState({})
-  
+  const [post, setPost] = useState(null)
+  const [comments, setComments] = useState([])
   const {post_id} = useParams()
   const {user_id} = useParams()
 
@@ -13,15 +13,31 @@ const CommentView = () => {
     console.log(selectedPost.data)
     setPost(selectedPost.data)
   }
-
+  const getComments = async () => {
+    const pComments = await Client.get(`/api/comment/comments/${post_id}`)
+    console.log(pComments.data)
+    setComments(pComments.data)
+  }
+  
   useEffect(() => {
     test()
+    
   }, [])
-
+useEffect(() => {
+  getComments()
+}, [post_id])
   return (
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <img src={post.imgRes}/>
-      <p style={{color: 'white'}}>{post.capRes}</p>
+      <p style={{color: 'white'}}>{post?.User?.username}</p>
+      <img src={post?.imgRes}/>
+      <p style={{color: 'white'}}>{post?.capRes}</p>
+      {post?.Comments?.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.comRes}</p>
+        </div>
+      ))}
+      
+    
 
     </div>
   )
